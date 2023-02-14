78e3ac619fe028fa9a9a1822efd7fb2a339e3b42
Jessica Swenson <jessicamswens@gmail.com>
2023-02-14 07:15:04 -0700

    added top and next components

---
+++ src/Next.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Next = () => {
+
+    return (
+        <div>
+            <p>This is the next component</p>
+        </div>
+    )
+};
+
+export default Next;
---
+++ src/Top.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Top = () => {
+
+    return (
+        <div>
+            <p>This is the Top component</p>
+        </div>
+    )
+};
+
+export default Top;
