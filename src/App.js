69ff9f0fbd465f66f027c52cb6e139d7f73ce11d
Jessica Swenson <jessicamswens@gmail.com>
2023-02-16 18:15:23 -0700

    add new profile functionality added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Profile from './components/Profile';
 
 
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [profiles, setProfiles] = useState([
     {
       firstName: 'Jessica',
@@ -18,16 +20,33 @@ function App() {
       firstName: 'Joely',
       lastName: 'Vernier',
     }
-  ])
+  ]);
+
+  const addNewProfile = (e) => {
+    e.preventDefault()
+    const newProfile = {
+      firstName,
+      lastName
+    }
+
+    setProfiles([...profiles, newProfile])
+    setFirstName('');
+    setLastName('');
+  }
 
   const profileDisplay = profiles.map((profileObj) => {
-    console.log();
     return <Profile info={profileObj} />
   })
 
   return (
     <div className="App">
       {profileDisplay}
+
+      <form onSubmit={addNewProfile}>
+        <input placeholder='first name' value={firstName} onChange={e => setFirstName(e.target.value)} />
+        <input placeholder='last name' value={lastName} onChange={e => setLastName(e.target.value)} />
+        <button>Add Profile</button>
+      </form>
     </div>
   );
 }
